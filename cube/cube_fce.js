91bdc91a0174dff779174e0310cc5c0eff4f078b
Gabe Henderson <throwport@gmail.com>
2016-02-02 21:10:16 -0500

    Mesh for Project 2

---
+++ cube/cube_fce.js
@@ -0,0 +1,15 @@
+//grab vertices, to desribe triangle face
+var F=[ 
+	[0,  1,  2], // front (red)
+	[0,  2,  3], // front (red)
+    [4,  5,  6], // back  (red)   
+    [4,  6,  7], // back  (red)
+    [8,  9,  10], // top  (yellow)  
+    [8,  10, 11], // top  (yellow)
+    [12, 13, 14], // bottom (yellow)   
+    [12, 14, 15], // bottom (yellow)
+    [16, 17, 18], // right  (blue)  
+    [16, 18, 19], // right  (blue)
+    [20, 21, 22], // left    (blue)
+    [20, 22, 23]  // left    (blue)
+  ];    
